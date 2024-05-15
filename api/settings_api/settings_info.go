75ad58a1ae2b7d8da6f56130dd204e20d0ebcb39
half-white <jyangzi163.com@qq.com>
2024-05-15 22:17:23 +0800

    封装响应

--- api/settings_api/settings_info.go
+++ api/settings_api/settings_info.go
@@ -1,7 +1,13 @@
 package settings_api
 
-import "github.com/gin-gonic/gin"
+import (
+	"gvb_server/models/res"
+
+	"github.com/gin-gonic/gin"
+)
 
 func (SettingsApi) SettingsInfoView(c *gin.Context) {
-	c.JSON(200, gin.H{"msg": "xxx"})
+	res.OkWithData(map[string]string{
+		"id": "xxx",
+	}, c)
 }
---
+++ models/res/response.go
@@ -0,0 +1,58 @@
+package res
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Response struct {
+	Code int    `json:"code"`
+	Data any    `json:"data"`
+	Msg  string `json:"msg"`
+}
+
+const (
+	Success = 0
+	Error   = 7
+)
+
+// 调用响应，实例化
+func Result(code int, data any, msg string, c *gin.Context) {
+	c.JSON(http.StatusOK, Response{
+		Code: code,
+		Data: data,
+		Msg:  msg,
+	})
+}
+
+// 如果响应成功
+func Ok(data any, msg string, c *gin.Context) {
+	Result(Success, data, msg, c)
+}
+
+func OkWithData(data any, c *gin.Context) {
+	Result(Success, data, "成功", c)
+}
+
+func OkWithMessage(msg string, c *gin.Context) {
+	Result(Success, map[string]any{}, msg, c)
+}
+
+// 如果响应失败
+func Fail(data any, msg string, c *gin.Context) {
+	Result(Error, data, msg, c)
+}
+
+func FailWithMessage(msg string, c *gin.Context) {
+	Result(Error, map[string]any{}, msg, c)
+}
+
+func FailWithCode(code ErrorCode, c *gin.Context) {
+	msg, ok := ErrorMap[code]
+	if ok {
+		Result(int(code), map[string]any{}, msg, c)
+		return
+	}
+	Result(Error, map[string]any{}, msg, c)
+}
