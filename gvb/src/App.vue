c455a64605900ef06f8a56216319d37ecb3e685e
half-white <jyangzi163.com@qq.com>
2024-07-10 23:26:28 +0800

    配置vue

---
+++ gvb/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <button v-html="home"></button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            home:"<a href='http://127.0.0.1:8082/hello'>后端接口</a>"
+        }
+    },
+}
+
+</script>
