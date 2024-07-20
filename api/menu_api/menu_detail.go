1348cb2e70deeeaea29638907e722a7055cbf15e
half-white <jyangzi163.com@qq.com>
2024-07-20 13:55:00 +0800

    完成菜单管理功能

---
+++ api/menu_api/menu_detail.go
@@ -0,0 +1,43 @@
+package menu_api
+
+import (
+	"gvb_server/global"
+	"gvb_server/models"
+	"gvb_server/models/res"
+
+	"github.com/gin-gonic/gin"
+)
+
+//单个菜单详情查阅
+
+func (MenuApi) MenuDetailView(c *gin.Context) {
+	//先查询菜单
+	id := c.Param("id")
+	var menuModel models.MenuModel
+	err := global.DB.Take(&menuModel, id).Error
+	if err != nil {
+		res.FailWithMessage("菜单不存在", c)
+		return
+	}
+
+	//查询连接表
+	var menuBanners []models.MenuBannerModel
+	global.DB.Preload("BannerModel").Order("sort desc").Find(&menuBanners, "menu_id in ?", id)
+
+	var banners = make([]Banner, 0)
+	for _, banner := range menuBanners {
+		if menuModel.ID != banner.MenuID {
+			continue
+		}
+		banners = append(banners, Banner{
+			ID:   banner.BannerID,
+			Path: banner.BannerModel.Path,
+		})
+	}
+	menuResponse := MenuResponse{
+		MenuModel: menuModel,
+		Banners:   banners,
+	}
+	res.OkWithData(menuResponse, c)
+	return
+}
--- routers/menu_router.go
+++ routers/menu_router.go
@@ -10,4 +10,5 @@ func (router RouterGroup) MenuRouter() {
 	router.GET("menu_names", app.MenuNameList)
 	router.PUT("menus/:id", app.MenuUpdateView)
 	router.DELETE("menus", app.MenuRemoveView)
+	router.GET("menus/:id", app.MenuDetailView)
 }
